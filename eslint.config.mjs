2148040433ce6707738bf68e92d5a65a7f45caee
Revillo-Store <cristian.serban33@yahoo.com>
2025-10-10 22:47:32 +0100

    missed one file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,26 +10,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
   },
   {
     rules: {
-      indent: ["error", 2],
-      'comma-dangle': ["error", "always-multiline"],
-      'semi': ["error", "always"],
-      'quotes': ["error", "single", { "avoidEscape": true }],
-      'object-curly-spacing': ["error", "always"],
-      'array-bracket-spacing': ["error", "never"],
-      'no-multiple-empty-lines': ["error", { "max": 1 }],
-      'eol-last': ["error", "always"],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'no-multiple-empty-lines': ['error', { 'max': 1 }],
+      'eol-last': ['error', 'always'],
     },
   },
 ];
