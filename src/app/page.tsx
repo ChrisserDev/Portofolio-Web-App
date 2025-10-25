0390c004da497eac56867fb3b22502ebd83fd32f
Revillo-Store <cristian.serban33@yahoo.com>
2025-10-25 13:53:43 +0100

    Update projects link path in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             Right now, I&apos;m diving into  <span className='desc-skill'>Next.js</span> and learning how to layer <span className='desc-skill'>TypeScript</span> more effectively across my projects. <br/><br/>
             If you&apos;d like to see what I&apos;ve been working on, follow my work log on <span><a className='github-link desc-skill' href="https://github.com/ChrisserDev" target='_blank' aria-label="Link to my personal Github profile" title="Link to my personal Github profile">Github</a></span> and updates on <span><a className='linkedin-link desc-skill' href="https://www.linkedin.com/in/cristianserban24/"  target='_blank' aria-label="Link to my personal Linkedin profile" title="Link to my personal Linkedin profile">LinkedIn</a></span> where I share progress, small wins, and experiments as I keep building and learning.
           </p>
-          <Link className='browse-projects' href="/projects">Browse Projects</Link>
+          <Link className='browse-projects' href="/projectss">Browse Projects</Link>
           <a className='resume-link' href="https://drive.google.com/file/d/126djPfCD_RtTBPb2koJ4rmiaWoVzFfWV/view?usp=sharing" target='_blank' rel="noopener noreferrer" aria-label='Link to Cristian Resume.'>Resume</a>
         </div>
         <div className='portrait-image-wrapper'>
